32ed3977fb03c74229ef4ae4ea1fc301cba512b2
shi116422-cpu <shi116422@gmail.com>
2026-03-10 10:09:50 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  api: {
-    bodyParser: {
-      sizeLimit: '500mb',
-    },
-    responseLimit: '500mb',
-  },
-}
-
-module.exports = nextConfig
+};
+module.exports = nextConfig;
